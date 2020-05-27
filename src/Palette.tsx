d1731a66672bed3d8347ca814fb075dec5dffcd9
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-05-27 15:11:43 +0200

    Add snackbar

--- src/Palette.tsx
+++ src/Palette.tsx
@@ -4,16 +4,24 @@ import { ExtendedPaletteT, LevelT, ColorModelT } from "./colorHelper";
 import { ColorBox } from "./ColorBox";
 import "rc-slider/assets/index.css";
 import { Navbar } from "./Navbar";
+import { Snackbar, IconButton } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close"
 
 type PalettePropsT = {
   paletteData: ExtendedPaletteT;
 };
 
-
 export const Palette: React.FC<PalettePropsT> = (props) => {
   const [saturation, setSaturation] = useState(500 as LevelT);
   const [colorModel, setColorModel] = useState("hex" as ColorModelT);
+  const [snackbarInfo, setSnackbarInfo] = useState("Hello");
   let palette = props.paletteData.colors[saturation];
+
+  const handleColorCodingChange = (coding: ColorModelT) => {
+    setColorModel(coding);
+    setSnackbarInfo(`Color format is set to ${coding.toLocaleUpperCase()}`)
+  }
+
   return (
     <PaletteStyled>
       {/* header */}
@@ -23,7 +31,7 @@ export const Palette: React.FC<PalettePropsT> = (props) => {
         level={saturation}
         onChange={(value: LevelT) => setSaturation(value)}
         colorMode={colorModel}
-        colorCodingChangeHandler={setColorModel}
+        colorCodingChangeHandler={handleColorCodingChange}
       />
 
       {/* color boxes */}
@@ -33,6 +41,22 @@ export const Palette: React.FC<PalettePropsT> = (props) => {
         ))}
       </div>
       {/* footer */}
+      {/* Snackbar with Materiall IU */}
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={snackbarInfo !== ""}
+        onClose={() => setSnackbarInfo("")}
+        message={<span id='message-id'>{snackbarInfo}</span>}
+        ContentProps={{
+          "aria-describedby": "message-id"
+        }}
+        action={[
+          <IconButton color='inherit' key='close' aria-lavel='close' >
+            <CloseIcon onClick={()=>setSnackbarInfo("")} />
+          </IconButton>
+        ]}
+        autoHideDuration={3000}
+      />
     </PaletteStyled>
   );
 };
